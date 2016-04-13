d224c2c1a4c366888557a27bfba99da71bbbb8b2
Michelangelo <dragonbe+github@gmail.com>
2016-04-14 01:06:18 +0530

    Fixing PDO statements

--- src/Model/PersonGateway.php
+++ src/Model/PersonGateway.php
@@ -12,11 +12,18 @@ class PersonGateway
         $this->pdo = $pdo;
     }
     
+    public function fetchAll()
+    {
+        $sql = 'SELECT * FROM `' . self::tableName . '`';
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    
     public function findByName($name)
     {
         $sql = 'SELECT * FROM `' . self::tableName . '` WHERE `name` = ?';
         $stmt = $this->pdo->prepare()
-        $result = $stmt->execute(array ($name));
-        return $result;
+        $stmt->execute(array ($name));
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 }
